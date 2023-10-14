58fa400243cd23a0563c29a1f95d2ac007242994
Soumya mishra <soumya9589@gmail.com>
2023-10-14 11:56:45 +0530

    loginlogout

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path=require('path');
+const bodyparser = require("body-parser");
+const session=require('express-session')
+const{v4:uuidv4}=require("uuid");
+
+const router = require('./router');
+const app=express()
+
+const port=process.env.PORT||3000;
+// body-parser middleware.
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+
+
+
+// view Engine setup.
+app.set('view engine','ejs');
+
+
+// load static assests
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.use('/assets',express.static(path.join(__dirname,'public/assets')));
+
+
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}));
+
+app.use('/route',router);
+
+// home route
+app.get('/',(req,res)=>{
+    res.render('base',{title:"Login Syestem"})
+})
+
+app.listen(port,()=>{console.log("Listening to the server on http:localhost:3000")})
